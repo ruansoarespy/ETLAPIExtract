19bd61da4b2c86b15dd493ad2bff59495e6f55ba
Ruan Ferreira Soares <ruanferreirasoares3@gmail.com>
2025-11-04 20:07:24 -0300

    salvando pipeline part 00

---
+++ src/pioeline_00.py
@@ -0,0 +1,28 @@
+import requests
+
+def extrair_dados_bitcoin():
+    """Obtém o preço atual do Bitcoin na Coinbase."""
+    url = 'https://api.coinbase.com/v2/prices/spot'
+    resposta = requests.get(url)
+    dados = resposta.json()
+    return dados
+
+def tratar_dados_bitcoin(dados_json):
+    """Transforma os dados brutos da API, renomeia colunas e adiciona timestamp."""
+    valor = dados_json['data']['amount']
+    criptomoeda = dados_json['data']['base']
+    moeda = dados_json['data']['currency']
+
+    dados_tratados = [{
+        "valor": valor,
+        "criptomoeda": criptomoeda,
+        "moeda": moeda
+        }]
+    
+    return dados_tratados
+
+if __name__ == "__main__":
+
+    dados_json = extrair_dados_bitcoin()
+    dados_tratados = tratar_dados_bitcoin(dados_json)
+    print(dados_tratados)
